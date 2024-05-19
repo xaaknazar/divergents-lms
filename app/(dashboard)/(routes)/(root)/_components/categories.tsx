6f1dd300547ef881c963cc7bf84e012703e000a5
xaaknazar <123650745+xaaknazar@users.noreply.github.com>
2024-05-19 17:09:36 +0500

    Add files via upload

---
+++ app/(dashboard)/(routes)/(root)/_components/categories.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Category } from "@prisma/client";
+import {
+  FcConferenceCall,
+  FcReading,
+  FcStatistics,
+  FcSportsMode,
+  FcMindMap,
+  FcServices
+} from 'react-icons/fc';
+import { IconType } from 'react-icons';
+
+import { CategoryItem } from "./category-item";
+
+interface CategoriesProps {
+  items: Category[];
+}
+
+const iconMap: Record<Category["name"], IconType> = {
+  "Лидерство": FcSportsMode,  
+  "Саморазвитие": FcReading,
+  "Менеджмент": FcStatistics,
+  "⁠Отношения": FcConferenceCall,
+  "⁠Мышление": FcMindMap,
+  "Навыки": FcServices
+};
+
+export const Categories = ({
+  items,
+}: CategoriesProps) => {
+  return (
+    <div className="flex items-center gap-x-3 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  )
+}
