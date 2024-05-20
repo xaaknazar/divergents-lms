a602099aef16a405ba5a15d90b84992afb9a7a74
xaaknazar <123650745+xaaknazar@users.noreply.github.com>
2024-05-20 10:45:37 +0500

    Update category-item.tsx

--- app/(dashboard)/(routes)/(root)/_components/category-item.tsx
+++ app/(dashboard)/(routes)/(root)/_components/category-item.tsx
@@ -46,13 +46,51 @@ export const CategoryItem = ({
     <button
       onClick={onClick}
       className={cn(
-        "py-2 px-3 text-sm bg-muted rounded-md flex items-center gap-x-1 hover:bg-slate-200 transition cursor-pointer",
-        isSelected && "border-sky-700 bg-sky-500/10 text-sky-800"
+        "py-2 px-3 text-sm bg-white rounded-md flex items-center gap-x-1 hover:shadow-sm transition cursor-pointer border",
+        isSelected && "border-sky-800/10 bg-sky-500/10 text-sky-800"
       )}
       type="button"
     >
       <div className="truncate text-sm font-semibold">
         {label}
+      </div>
+    </button>
+  )
+}
+
+export const AllCategoriesButton = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const currentCategoryId = searchParams.get("categoryId");
+  const currentTitle = searchParams.get("title");
+
+  const isSelected = currentCategoryId === null;
+
+  const onClick = () => {
+    const url = qs.stringifyUrl({
+      url: pathname,
+      query: {
+        title: currentTitle,
+        categoryId: null,
+      }
+    }, { skipNull: true, skipEmptyString: true });
+
+    router.push(url);
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "py-2 px-3 text-sm rounded-md flex items-center gap-x-1 hover:shadow-sm transition cursor-pointer border",
+        isSelected ? "border-sky-800/10 bg-sky-500/10 text-sky-800" : "bg-white"
+      )}
+      type="button"
+    >
+      <div className="truncate text-sm font-semibold">
+        Все
       </div>
     </button>
   )
