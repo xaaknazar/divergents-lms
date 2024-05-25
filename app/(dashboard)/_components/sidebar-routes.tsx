82aec9bb853bae605341ddd242bc0a0809bf55d6
xaaknazar <123650745+xaaknazar@users.noreply.github.com>
2024-05-25 22:30:35 +0500

    Update sidebar-routes.tsx

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,50 +1,56 @@
 "use client";
 
-import { LucideIcon } from "lucide-react";
-import { usePathname, useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
+import { BarChart, Compass, File, Layout, List, Sidebar } from "lucide-react"
+import { SidebarItem } from "./sidebar-item";
+import { usePathname } from "next/navigation";
 
-interface SidebarItemProps {
-  icon: LucideIcon;
-  label: string;
-  href: string;
-}
+const guestRoutes = [
+    {
+        icon: Compass,
+        label: "Обзор",
+        href: "/",
+        },
+    {
+        icon: Layout,
+        label: "Мои курсы",
+        href: "/search",
+    },
+    {
+        icon: File,
+        label: "Reports",
+        href: "/reports",
+        },
+];
 
-export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
-  const pathname = usePathname();
-  const router = useRouter();
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Курсы",
+        href: "/teacher/courses",
+        },
+        {
+            icon: BarChart,
+            label: "Аналитика",
+            href: "/teacher/analytics",
+        },
 
-  const isActive =
-    (pathname === "/" && href === "/") ||
-    pathname === href ||
-    pathname?.startsWith(`${href}/`);
+]
 
-  const onClick = () => {
-    router.push(href);
-  };
+export const SidebarRoutes = () => {
+    const pathname = usePathname();
+    const isTeacherPage = pathname?.includes("/teacher");
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
-  return (
-    <button
-      onClick={onClick}
-      type="button"
-      className={cn(
-        "flex w-full text-sm items-center py-3.5 px-3 my-1 hover:bg-muted rounded-lg transition-background group", // добавлено my-2 для увеличения расстояния
-        isActive ? "bg-muted text-primary font-medium" : "text-muted-foreground"
-      )}
-    >
-      <Icon
-        size={26}
-        className={cn(
-          "mr-2",
-          isActive ? "text-primary animate-spin-once" : "text-muted-foreground"
-        )}
-      />
-      {label}
-      {label === "Reports" && (
-        <div className="inline-flex items-center border rounded-md px-2.5 py-0.5 text-xs font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-sky-500/10 text-sky-800 ml-auto">
-          Скоро
+    return (
+        <div className="flex flex-col w-full">
+            {routes.map((route) =>(
+                <SidebarItem
+                key={route.href}
+                icon={route.icon}
+                label={route.label}
+                href={route.href}/>
+
+            ))}
         </div>
-      )}
-    </button>
-  );
-};
+    )
+}
