c1acd0f8b45e2502e90c9390922de071218b4bc8
xaaknazar <123650745+xaaknazar@users.noreply.github.com>
2024-06-24 16:42:02 +0500

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
+import { CourseFooter } from "./_components/course-footer";
 
 const DashboardLayout = ({
     children
@@ -7,17 +8,19 @@ const DashboardLayout = ({
     children: React.ReactNode;
 }) => {
     return (
-        <div className="h-full">
+        <div className="h-full flex flex-col">
             <div className="h-[92px] md:pl-72 fixed inset-y-0 w-full z-50">
                 <Navbar />
             </div>
             <div className="hidden md:flex h-full w-72 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
-            {/* Использование произвольного значения для максимальной ширины, чтобы сделать основное содержимое чуть меньше, чем 8xl */}
-            <main className="md:pl-72 pt-[85px] max-w-[124rem]">
+            <main className="flex-1 md:pl-72 pt-[85px] max-w-[124rem] mx-auto w-full">
                 {children}
             </main>
+            <div className="md:pl-72">
+                <CourseFooter />
+            </div>
         </div>
     );
 }
